dee24cc6aa336a94421ac9d439b227305d3c460d
sartra <strenshaw@gmail.com>
2016-11-06 16:55:26 -0500

    working with scales to creat axis

--- area.js
+++ area.js
@@ -1,6 +1,11 @@
 var dataArray = [25, 26, 28, 33, 35, 45, 53, 70, 88, 91, 95, 111, 121, 144, 166, 171, 181];
 
-var dataYears = ['2000','2001','2002','2003','2004','2005','2006','2007','2007','2008','2009','2009','2010','2011','2012','2013','2014'];
+var dataYears = ['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014', '2015', '2016'];
+
+var parseDate = d3.timeParse("%Y");
+console.log(d3.max(dataYears, function(d) { return parseDate(d); }));
+console.log(d3.extent(dataYears, function(d) { return parseDate(d); })); //givese min and max useful for domain of x axis 
+
 
 var height = 200;
 var width = 500;
@@ -11,21 +16,33 @@ var y = d3.scaleLinear()
           .domain([0, d3.max(dataArray)])  
           .range([height, 0]);
 
+var x = d3.scaleTime()
+          .domain(d3.extent(dataYears, function(d) { return parseDate(d); }))  
+          .range([0, width]);
+
+console.log(x(parseDate('2010'))); // will give the position on the x scale for the year 2010
 
 var yAxis = d3.axisLeft(y).ticks(3).tickPadding(10).tickSize(10);
 
+var xAxis = d3.axisBottom(x);
+
 var area = d3.area()
-              .x( function (d,i){ return i*20; })
-              .y(height)
+              .x( function (d,i){ return x(parseDate(dataYears[i])); })
+              .y0(height)
               .y1( function(d){ return y(d); });
 
 var svg = d3.select("body").append("svg").attr("height","100%").attr("width", "100%");
 
-var chartGroup = svg.append("g").attr("transform","translate("+margin.left+","+margin.top+")")
+var chartGroup = svg.append("g").attr("transform","translate("+margin.left+","+margin.top+")");
 
 chartGroup.append("path").attr("d", area(dataArray));
 
 chartGroup.append("g")
     .attr("class","axis y")
     .call(yAxis);
+chartGroup.append("g")
+    .attr("class","axis x")
+    .attr("transform","translate(0,"+height+")")
+    .call(xAxis);
+    
 
--- shapes.js
+++ shapes.js
@@ -1,9 +1,44 @@
 var dataArray = [5, 11, 18];
+var dataDays = ["Mon","Wed","Fri"];
+
+var rainbow = d3.scaleSequential(d3.interpolateRainbow).domain([0, 10]);
+
+var rainbow2 = d3.scaleSequential(d3.interpolateRainbow).domain([0, 3]);
+
+
+
+
+var x = d3.scalePoint()
+          .domain(dataDays)
+          .range([0,170]); // scale Point take min and max pixel values; good for point charts, not bar graphs 
+
+
+var x = d3.scaleBand()
+          .domain(dataDays) 
+          .range([0,170]) // scale Band better for bar chart; takes min and max 
+          .paddingInner(0.1176); // takes 3rd argument padding - determines the percentage of chart dedicated to white space 
+
+  // API FOR SCALE BAND: https://goo.gl/186ghj  
+
+  var x = d3.scaleOrdinal()
+          .domain(dataDays)
+          .range([35, 95, 155]); //scale ordinal takes specific points
+
+
+
+var xAxis = d3.axisBottom(x);
 
 var svg = d3.select("body").append("svg").attr("height","100%").attr("width","100%");
 
-// var color = "#"+333;
-// console.log(color);
+var cat20 = d3.schemeCategory20;
+console.log(cat20);
+
+// ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"]
+
+svg.append("g")
+    .attr("class", "x axis hidden")
+    .attr("transform", "translate(0,300)")
+    .call(xAxis);
 
 
 svg.selectAll("rect")
@@ -11,7 +46,8 @@ svg.selectAll("rect")
   .enter().append("rect")
           .attr("height", function(d,i){return d*15;})
           .attr("width","50")
-          .attr("fill", "pink") 
+          // .attr("fill", "pink")
+          .attr("fill", function(d,i){return rainbow(i)}) 
           .attr("x",function(d, i){ return 60*i +10; })
           .attr("y",function(d,i){ return 300-(d*15); });
 
@@ -23,6 +59,7 @@ svg.selectAll("circle.first")
           .attr("class","first")
           .attr("cx",function(d,i){newX+=(d*3)+(i*20); return newX})
           .attr("cy", "100")
+          .attr("fill", function(d,i){return rainbow2(i)}) 
           .attr("r", function(d){ return d*3});
 
 
@@ -32,6 +69,7 @@ svg.selectAll("ellipse.second")
   .enter().append("ellipse")
           .attr("class","second")
           .attr("cx",function(d,i){newX+=(d*3)+(i*20);return newX})
+          .attr("fill", function(d,i){ return cat20[i] })
           .attr("cy", "100")
           .attr("rx", function(d){ return d*3})
           .attr("ry", "30");
